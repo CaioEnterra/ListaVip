94815c20fc9357665aeaed93e1d970378d12e288
CaioEnterra <enterra.caio@gmail.com>
2018-06-12 19:18:00 -0300

    cadastrando e listando evento e pessoa ainda não exclui

--- app/src/main/java/com/example/ascom_unitins/listavip/View/TelaSobre.java
+++ app/src/main/java/com/example/ascom_unitins/listavip/View/TelaSobre.java
@@ -1,12 +1,8 @@
 package com.example.ascom_unitins.listavip.View;
 
-import android.app.Fragment;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.ascom_unitins.listavip.R;
 
